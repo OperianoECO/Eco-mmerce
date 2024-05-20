0f994a4fd7231c96340f6accccc186669b489b7e
bingtai20 <taibing20@gmail.com>
2024-05-20 22:11:11 +0800

    commit store + base

--- __init__.py
+++ __init__.py
@@ -7,5 +7,10 @@ app.config['SECRET_KEY'] = 'secret_key'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 allowed_extensions_list = ['jpg', 'png', 'jpeg', '']
 
+
+@app.route('/')
+def home():
+    return render_template('store.html')
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
